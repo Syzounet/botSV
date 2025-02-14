950d3de6b03fad3d8f9898568d733b70b17cc5cc
souquetg <gauthier.souquet@etu.umontpellier.fr>
2025-02-14 16:47:21 +0100

    fix: bug

--- bot.py
+++ bot.py
@@ -59,7 +59,7 @@ async def V(ctx):
     await ctx.send("Tu dois savoir, que quoi qu'il arrive, je serais toujours la pour toi parce qu'on forme une putain d'équipe. Nous deux contre le monde, parce que toi, c'est moi et moi c'est toi. Parce que toi et moi c'est vrai c'est absolument authentique tellement que ça brûle dans mon coeur, mon amour. j'aime cette histoire qu'on est entrain d'écrire et je te fais la promesse que je ne laisserais pas ce monde briser ta douceur ")
     await ctx.send("*Joyeuse Saint-Valentin mon baybay ❤️ je t'aime")
     await asyncio.sleep(5)
-    await ctx.send("oh et attend je sais pas si tu as remarqué mais les lettre forme une phrase spéciale avec comme dernière lettre V :")
+    await ctx.send("oh et attend je sais pas si tu as remarqué mais les lettre forme une phrase spéciale :")
     await ctx.send("W Y B M V ? : essaie de le traduire (indice : c'est en anglais)")
 
 bot.run(TOKEN)
